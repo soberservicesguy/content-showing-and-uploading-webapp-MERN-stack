fd63b3d6b67ec9e3be5e67492bad5654a679e8be
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-04-10 20:54:50 +0500

    worked more

--- backend/routes/uploads/bulk_blogpost_upload.js
+++ backend/routes/uploads/bulk_blogpost_upload.js
@@ -160,7 +160,7 @@ router.post('/bulk-upload-blogposts', passport.authenticate('jwt', { session: fa
 	// timestamp = Date.now()
 	timestamp = new Date()
 	currentDate = timestamp.toLocaleDateString("en-US").split("/").join(":");
-	currentTime = timestamp.toLocaleTimeString("en-US", { hour12: false }).split("/").join("|");
+	currentTime = timestamp.toLocaleTimeString("en-US").split("/").join("|");
 
 	bulk_upload_blogposts( `${currentDate}_${currentTime}`, 'bulk_blogposts' )(req, res, (err) => {
 		if(err){
@@ -211,14 +211,24 @@ router.post('/bulk-upload-blogposts', passport.authenticate('jwt', { session: fa
 				let all_images_db_objects = []
 				Promise.all(images.map(async (image_file) => {
 
-					console.log('FILEPATH WHERE IMAGES PATH IS GOING TO BE')
-					console.log(get_file_path_to_use_for_bulk_files(`${currentDate}_${currentTime}`,'bulk_blogposts', image_file.originalname))
+					let image_filepath_value
+
+					if (use_gcp_storage || use_aws_s3_storage){
+
+						image_filepath_value = `bulk_blogposts/${currentDate}_${currentTime}/${image_file.originalname}`
+
+					} else {
+
+						image_filepath_value = get_file_path_to_use_for_bulk_files(`${currentDate}_${currentTime}`,'bulk_blogposts', image_file.originalname)
+
+					}
 
 					const newImage = new Image({
 						_id: new mongoose.Types.ObjectId(),
 						category: 'blogpost_image',
 						// image_filepath: `${get_filepath_to_save_with_bulk_uploading('bulk_blogposts', `${currentDate}_${currentTime}`)}${image_file.originalname}`,
-						image_filepath: get_file_path_to_use_for_bulk_files(`${currentDate}_${currentTime}`,'bulk_blogposts', image_file.originalname),
+						// image_filepath: get_file_path_to_use_for_bulk_files(`${currentDate}_${currentTime}`,'bulk_blogposts', image_file.originalname),
+						image_filepath: image_filepath_value,
 						// image_filepath: get_file_path_to_use(image_file, 'blogpost_image_main', timestamp),
 						title: image_file.originalname,
 						object_files_hosted_at: get_file_storage_venue(),
