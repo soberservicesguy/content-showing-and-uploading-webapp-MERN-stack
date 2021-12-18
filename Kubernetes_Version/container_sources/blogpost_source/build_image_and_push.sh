f4173832aef3f5711560b22fdb3ab55e38eb6dd8
soberservicesguy <arsalan.ahmad.ishaq@gmail.com>
2021-12-18 19:25:26 +0500

    worked more

--- Kubernetes_Version/container_sources/blogpost_source/build_image_and_push.sh
+++ Kubernetes_Version/container_sources/blogpost_source/build_image_and_push.sh
@@ -21,6 +21,6 @@
 
 #!/usr/bin/env bash
 
-docker image build -t content_blogpost_175 .
-docker image tag content_blogpost_175 soberservicesguy/portfolio-images:content_blogpost_175
-docker image push soberservicesguy/portfolio-images:content_blogpost_175
+docker image build -t content_blogpost .
+# docker image tag content_blogpost_175 soberservicesguy/portfolio-images:content_blogpost_175
+# docker image push soberservicesguy/portfolio-images:content_blogpost_175
